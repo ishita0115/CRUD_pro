98cc77a47d0ac78b680c0469495d10b6c0d2eb6a
ishita <ishitachovatiya15@gmail.com>
2024-02-22 12:50:53 +0530

    login and register done

--- webapp/url.py
+++ webapp/url.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home,name=''),
+    path("register/",views.register,name='register'),
+    path('my_login/',views.my_login,name='my_login')
 ]
