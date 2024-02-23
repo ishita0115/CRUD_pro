5ec43d30d8af79a02aa1f97ebfe091c28b31485e
ishita <ishitachovatiya15@gmail.com>
2024-02-23 16:55:23 +0530

    update and delete add

--- webapp/url.py
+++ webapp/url.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('dashboard/',views.dashboard,name='dashboard'),
     path("signout/",views.signout,name='logout'),
     path('create-record/',views.create_record,name='create-record'),
+    path('update-record/<int:pk>',views.update_record,name='update-record'),
+    path('record-view/<int:pk>',views.singular_record,name='view-record'),
+    path('delete-record/<int:pk>',views.delete_record,name='delete-record')
 ]
